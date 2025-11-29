a380d0f5dee005fd737ed6cea970e1320037b5d6
Aarman K Kadri <aarman1922030@gmail.com>
2025-11-29 13:12:46 +0530

    Adding better and optimal solution of set matrix zero problem

--- Striver SDE Sheet/Array Part - I/SetMatrix.java
+++ Striver SDE Sheet/Array Part - I/SetMatrix.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class SetMatrix {
 
-    public void setZeroes(int[][] matrix) {
+    public void setZeroesBrute(int[][] matrix) {
         int m = matrix.length;
 
         int n = matrix[0].length;
@@ -30,11 +30,85 @@ public class SetMatrix {
 
     }
 
+    public void setZeroesBetter(int[][] matrix) {
+        int m = matrix.length; // Length of rows
+
+        int n = matrix[0].length;  // lenght of columns
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Next iteration for marking the row's and columns which are true to zero
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void setZeroesOptimal(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // col = matrix[0][..]
+        // row = matrix[..][0]
+
+        int col0 = 1;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    // marting ith row
+                    matrix[i][0] = 0;
+
+                    // marking jth col
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    }
+                    else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(matrix[i][j] != 0) {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0) {
+            for(int j=0; j<n; j++) matrix[0][j] = 0;
+        }
+
+        if(col0 == 0) {
+            for(int i=0; i<m; i++) matrix[i][0] = 0;
+        }
+    }
+
     public static void main(String[] args) {
-        int[][] matrix = {{1,1,1}, {1,0,1}, {1,1,1}};
+        int[][] matrix = {{1,2,3}, {4,0,6}, {7,8,9}};
 
         SetMatrix st = new SetMatrix();
-        st.setZeroes(matrix);
+        // st.setZeroesBrute(matrix);
+        // st.setZeroesBetter(matrix);
+        st.setZeroesOptimal(matrix);
 
         for(int[] row: matrix) {
             for(int val: row) {
